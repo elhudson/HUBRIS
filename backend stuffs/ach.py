12624ed362162845ed319ffe8a5c2e4b557ed365
El Hudson <67842840+elhudson@users.noreply.github.com>
2023-02-20 21:49:24 -0500

    started work on ability objects wheeeee

--- backend stuffs/ach.py
+++ backend stuffs/ach.py
@@ -18,6 +18,7 @@ class character:
         self.cha=df["cha"][0]
         self.ability_ids=df["abilities"][0]
 
+
 def is_relational(function_name):
     l=function_name.split()
     if len(l)==1:
@@ -37,7 +38,7 @@ def get_tables():
     for table in tables:
         if table!="characters":
             if is_relational(table):
-                pivot_tables.append(table)
+                pivot_tables.append(f'[{table}]')
             else:
                 property_tables.append(table)
     return pivot_tables, property_tables
@@ -46,13 +47,36 @@ relations=get_tables()[0]
 properties=get_tables()[1]
 
 
-def fetch_property(id):
-    cur=con.cursor()
-    for table in properties:
-        sql=f'SELECT * FROM {table} WHERE id="{id}"'
-        d=pd.read_sql_query(sql,con)
-        if d.empty==False:
-            break
+def fetch_by_id(id, is_property=True):
+    if is_property==True:
+        for table in properties:
+            sql=f'SELECT * FROM {table} WHERE id="{id}"'
+            d=pd.read_sql_query(sql,con)
+            if d.empty==False:
+                break
+    elif is_property==False:
+        for table in relations:
+            sql=f'SELECT * FROM {table} WHERE primary_key="{id}"'
+            d=pd.read_sql_query(sql,con)
+            if d.empty==False:
+                break
     rec=pd.DataFrame.from_records(d)
     return rec
        
+class ability:
+    def __init__(self,id):
+        self.rec=fetch_by_id(id)
+        self.name=self.rec["title"][0]
+        self.xp=self.rec["xp"][0]
+        self.desc=self.rec["description"][0]
+        self.id=id
+
+class feature(ability):
+    def __init__(self,id):
+        super().__init__(id)
+        
+class power(ability):
+    def __init__(self,id):
+        super().__init__(id)
+        self.pwr=self.rec["pwr"][0]
+        self.tree=self.rec["tree"][0]
